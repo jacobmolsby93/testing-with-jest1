34cf4d90dd7614b1504fbc4bf9cddb0b79dc4d9a
Jacob Molsby <jacobmolsby@hotmail.se>
2021-07-10 12:44:50 +0000

    trying to fix wrong

--- assets /scripts/tests/calc.test.js
+++ assets /scripts/tests/calc.test.js
@@ -1,10 +1,15 @@
 const { TestWatcher } = require("jest");
 const addition = require("../calc")
 
+
+
 describe('Calculator', () => {
     describe("Addition function", () => {
         test("should return 42 for 20 + 22", () => {
             expect(addition(20, 22)).toBe(42);
+        })
+        test("should return 73 for 42 + 31", () => {
+            expect(addition(42, 31)).toBe(73);
         })
     });
     describe("Subtraction function", () => {
@@ -18,3 +23,11 @@ describe('Calculator', () => {
     });
     
 })
+
+
+/*
+Mocking allows us to run part of our code against our tests.
+
+
+
+*/
